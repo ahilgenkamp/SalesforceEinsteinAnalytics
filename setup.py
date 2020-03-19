679746e8032c6b16e1f1ccefd182544d9ff201cb
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2020-03-19 17:54:57 -0400

    fixing issue in 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='SalesforceEinsteinAnalytics',
-    version='0.0.2',
+    version='0.0.3',
     author='Adam Hilgenkamp',
     author_email='ahilgie@gmail.com',
     description='Python package for working with the Einstein Analytics API',
