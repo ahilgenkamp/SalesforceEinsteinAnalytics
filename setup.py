f76da4fa14d6065ff5cf8f46681a25858a61cb18
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2020-03-19 15:07:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='salesforceEinsteinAnalytics',
+    name='SalesforceEinsteinAnalytics',
     version='0.0.1',
     author='Adam Hilgenkamp',
     author_email='ahilgie@gmail.com',
