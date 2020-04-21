3499a6567716109ae62f626097e7770584cb0b6e
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2020-04-21 13:03:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requirements = [
     
 setup(
     name='SalesforceEinsteinAnalytics',
-    version='0.0.5',
+    version='0.0.6',
     author='Adam Hilgenkamp',
     author_email='ahilgie@gmail.com',
     description='Python package for working with the Einstein Analytics API',
